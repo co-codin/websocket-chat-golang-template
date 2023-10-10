18b3969401b135ca1ee4b19dcbcfc3754fc727c0
elijah <cuiyeqing960904@gmail.com>
2023-10-10 07:39:19 -0400

    connect to ws

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"ws/internal/handlers"
 )
 
 func main() {
 	mux := routes()
 
-	log.Println("Staring web server on port 8080")
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
 
+	log.Println("Staring web server on port 8080")
 
 	_ = http.ListenAndServe(":8080", mux)
 }
